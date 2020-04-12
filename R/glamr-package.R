0e9c0b5aa446ffedb4b6401a6bd06c22aa900267
Aaron Chafetz <achafetz@usaid.gov>
2020-04-12 08:36:08 -0400

    define globalVariables

--- R/glamr-package.R
+++ R/glamr-package.R
@@ -1,6 +1,11 @@
 #' @keywords internal
 "_PACKAGE"
 
+if (getRversion() >= "2.15.1")
+  utils::globalVariables(
+    c("value", "Value", "ctime", "filepath")
+  )
+
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
