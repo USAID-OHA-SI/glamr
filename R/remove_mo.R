6cf7068152d48f4e44c667c25e30b13b68b359b5
Benjamin Kasdan <62769562+benjaminnk@users.noreply.github.com>
2021-07-21 13:44:18 -0400

    New function for removing M&O

---
+++ R/remove_mo.R
@@ -0,0 +1,20 @@
+#' A Function to Remove M&O from datasets
+#'
+#' @param df financial or comprehensive budget dataset
+#'
+#' @return a dataset excluding M&O
+#' @export
+#'
+
+remove_mo<-function(df){
+
+  #check that mechanism exists in MSD before starting (OUxIM or PSNUxIM, not PSNU)
+  if(("mech_code" %in% names(df) == FALSE)) {
+    stop('This dataset does not have mechanisms. Make sure it is OUxIM or PSNUxIM')
+  }
+
+  df<-df%>%
+    dplyr::filter(record_type =="Implementing Mechanism")
+  return(df)
+
+}
