ba00a0c35f5afed4f605f224d03eefef7e74e8d4
Aaron Chafetz <achafetz@usaid.gov>
2021-07-27 16:39:46 -0400

    update calendar to include open and close entry periods

--- R/data.R
+++ R/data.R
@@ -2,12 +2,13 @@
 #'
 #' A dataset the dates for the release of the MER Structured Dataset
 #'
-#' @format A data frame with 8 rows and 4 variables:
+#' @format A data frame with 8 rows and 5 variables:
 #' \describe{
 #'   \item{fiscal_year}{fiscal year, start = October}
 #'   \item{quarter}{fiscal quarter, integer 1-4}
 #'   \item{type}{data release type, initial or clean}
-#'   \item{release_date}{date data are available}
+#'   \item{entry_open}{date entry begins into DATIM}
+#'   \item{entry_close}{date DATIM is closed and data are frozen}
 #' }
 #' @source \url{https://datim.zendesk.com/hc/en-us/articles/115001940503-PEPFAR-Data-Calendar}
 "pepfar_data_calendar"
--- data-raw/stored_data.R
+++ data-raw/stored_data.R
@@ -4,16 +4,18 @@
 
 pepfar_data_calendar <-
   tibble::tribble(
-     ~fiscal_year, ~quarter,     ~type, ~release_date,
-            2021L,       1L, "initial",  "2021-02-12",
-            2021L,       1L,   "clean",  "2021-03-19",
-            2021L,       2L, "initial",  "2021-05-14",
-            2021L,       2L,   "clean",  "2021-06-18",
-            2021L,       3L, "initial",  "2021-08-13",
-            2021L,       3L,   "clean",  "2021-09-17",
-            2021L,       4L, "initial",  "2021-11-12",
-            2021L,       4L,   "clean",  "2021-12-17"
-     )
+    ~fiscal_year, ~quarter,     ~type,  ~entry_open, ~entry_close,
+           2021L,       1L, "initial", "2021-01-06", "2021-02-12",
+           2021L,       1L,   "clean", "2021-03-01", "2021-03-19",
+           2021L,       2L, "initial", "2021-04-01", "2021-05-14",
+           2021L,       2L,   "clean", "2021-06-01", "2021-06-18",
+           2021L,       3L, "initial", "2021-07-01", "2021-08-13",
+           2021L,       3L,   "clean", "2021-09-01", "2021-09-17",
+           2021L,       4L, "initial", "2021-10-01", "2021-11-12",
+           2021L,       4L,   "clean", "2021-12-01", "2021-12-17"
+    )
+
+
 
 
 usethis::use_data(pepfar_data_calendar, overwrite = TRUE)
