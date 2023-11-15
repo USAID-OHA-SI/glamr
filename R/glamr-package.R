1f50dc0e1336df4de5563463d53a9506ec6f3fda
Aaron Chafetz <achafetz@usaid.gov>
2023-11-15 16:59:32 -0500

    feat: specify globals

--- R/glamr-package.R
+++ R/glamr-package.R
@@ -3,7 +3,12 @@
 
 if (getRversion() >= "2.15.1")
   utils::globalVariables(
-    c("value", "Value", "ctime", "filepath")
+    c("value", "Value", "ctime", "filepath", ".", "clean_column",
+      "created_time", "drive_resource", "folderpath_tmp", "id", "kind",
+      "last_modifying_user_display_name", "modified_time", "mtime", "name",
+      "original_filename", "owners", "pepfar_country_xwalk", "period",
+      "permission_ids", "permissions", "permissions_allow_file_discovery",
+      "permissions_domain", "permissions_type", "trashed", "username", "x")
   )
 
 # The following block is used by usethis to automatically manage
